10185b42c58042375f8ae3e635ab200832111087
Giuseppe Chiesa <gchiesa@mobiquityinc.com>
2019-03-13 18:01:30 +0100

    Bump version: 1.2.0 → 1.3.0

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/gchiesa/pip_negotiator',
-    version='1.2.0',
+    version='1.3.0',
     zip_safe=False,
     entry_points={
         'console_scripts': [
